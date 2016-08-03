937e7bf43ce07cb5a46a1dfed00f4ca07444df50
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-08-03 11:43:19 -0700

    added ngResource

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,11 +1,12 @@
 namespace app.Controllers {
   export class HomeController {
     public movies;
-    constructor($http: ng.IHttpService) {
-      $http.get('api/movies').then((results) => {
-        console.log(results);
-        this.movies = results.data;
-      })
+    constructor($http: ng.IHttpService, private movieService: app.Services.MovieService) {
+      // $http.get('api/movies').then((results) => {
+      //   console.log(results);
+      //   this.movies = results.data;
+      // })
+      this.movies = this.movieService.list();
     }
   }
 
--- ngApp/js/services.ts
+++ ngApp/js/services.ts
@@ -1,3 +1,14 @@
 namespace app.Services {
+  export class MovieService {
+    private MovieResource;
 
+    public list() {
+      return this.MovieResource.query();
+    }
+
+    constructor(private $resource: ng.resource.IResourceService){
+      this.MovieResource = $resource('/api/movies/:id');
+    }
+  }
+  angular.module('app').service('movieService', MovieService);
 }
